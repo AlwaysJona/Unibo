3e9bbd4ba05bb57c7f454828d755919ad8b374c0
Jona <jonayed.ahmed@studio.unibo.it>
2024-06-02 10:38:29 +0200

    correction on formulas

--- random_animations/beats.py
+++ random_animations/beats.py
@@ -6,18 +6,19 @@ fig, ax = plt.subplots()
 
 
 time = 10000
-t = np.linspace(0, time, time)
+N = time*500
+t = np.linspace(0, time, N)
 
 v = 340 # wave speed
-w = 2*np.pi / 10 # angular frequency
+w = 2*np.pi * 10 # angular frequency
 k = w/v # wave number
 
-w1 = 2*np.pi / 11
+w1 = 2*np.pi * 11
 k1 = w1/v
 
-print(f"Wave length: {2*np.pi/k}")
+print(f"Wave length: {2*np.pi/k}\nPeriod: {2*np.pi/w}")
 
-x = np.linspace(0,100000,100000)
+x = np.linspace(0,500,1000)
 
 def progressive_wave(t,x,k,w):
     return np.sin(k*x - w*t)
@@ -26,10 +27,10 @@ line = ax.plot(x,progressive_wave(0,x,k,w))[0]
 ax.set_ylim([-3,3])
 
 def animation(frame):
-    line.set_data(x,progressive_wave(frame,x,k,w)+progressive_wave(frame,x,k1,w1))
+    line.set_data(x,progressive_wave(t[frame],x,k,w)+progressive_wave(t[frame],x,k1,w1))
     return line
 
-anim = FuncAnimation(fig,animation,time, interval = 20)
+anim = FuncAnimation(fig, animation, N, interval = 20)
 
 ax.set_xlabel("x")
 ax.set_ylabel("y")
